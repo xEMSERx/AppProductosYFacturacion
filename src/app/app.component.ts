ba26e3291646c1574d815684786463c88bd29fe9
EMSER <emser1901@gmail.com>
2025-06-20 23:37:53 -0300

    Solución de errores en ABM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [ProductListComponent]
 })
-export class AppComponent {
-  title = 'AppProductosYFacturacion';
-}
+export class AppComponent {}
