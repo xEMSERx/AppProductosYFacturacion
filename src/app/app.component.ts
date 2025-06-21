bab1599b119976b7840425c583f178676790d345
EMSER <emser1901@gmail.com>
2025-06-20 23:52:06 -0300

    Actualización de ABM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [ProductListComponent]
+  imports: [ProductListComponent, ProductFormComponent, ProductDetailComponent],
 })
 export class AppComponent {}
